831f54392a357a462736c2c188a66bc26ac89675
bnb <deathproooof@gmail.com>
2023-09-09 23:07:23 +0900

    追加:ちょいアルゴリズム

--- e05.js
+++ e05.js
@@ -1,4 +1,10 @@
 const array = ["猫1,猫2,猫3," , "猫1,猫3,猫4," , "猫1,猫2,猫5," , "猫2,猫3,猫4," , "猫1,猫4,猫5,"];
+let currentArray = [...array];
+
+function filterArray(value) {
+    currentArray = currentArray.filter(item => item.includes(value));
+    console.log(currentArray);
+}
 
 window.onload = ()=> {
     const containerAnd = document.getElementById('container_and');
@@ -12,10 +18,10 @@ window.onload = ()=> {
     const orSelectedTagInputted = containerOr.querySelector('.selected_tag_inputted');
 
     andTags.forEach((tag) => {
-        tag.addEventListener('click', () => selectTagToggleComplex(tag, andSelectedTagInputted, searchButton) );
+        tag.addEventListener('click', () => selectTagToggleAnd(tag, andSelectedTagInputted, searchButton) );
     });
     orTags.forEach((tag) => {
-        tag.addEventListener('click', () => selectTagToggleComplex(tag, orSelectedTagInputted, searchButton) );
+        tag.addEventListener('click', () => selectTagToggleOr(tag, orSelectedTagInputted, searchButton) );
     });
 
     clearButton.addEventListener('click', () => { 
@@ -31,7 +37,40 @@ window.onload = ()=> {
 };
 
 // タグの選択状態を切り替え、リンクをセットする関数
-function selectTagToggleComplex(tag, inputField, searchButton) {
+function selectTagToggleAnd(tag, inputField, searchButton) {
+    filterArray(tag.value)
+
+    if(currentArray.length!==0){
+        tag.classList.add('select_tag'); 
+        tag.classList.remove('exist'); 
+
+        const tagValue = encodeURIComponent(tag.value);
+        const selectedTags = inputField.value.split('+');
+
+        if (!selectedTags.includes(tagValue)) {
+            inputField.value += tagValue + "+";
+        } else {
+            inputField.value = inputField.value.replace(`${tagValue}+`, "");
+        };
+
+        const setLinkAnd = document.getElementById('container_and').querySelector('.selected_tag_inputted').value.replace(/\+$/, "");
+        const setLinkOr = document.getElementById('container_or').querySelector('.selected_tag_inputted').value.replace(/\+$/, "");
+
+        if (setLinkAnd !== "" && setLinkOr === ""){
+            searchButton.setAttribute('href', `results?and=${setLinkAnd}`)
+        } else if (setLinkAnd === "" && setLinkOr !== ""){
+            searchButton.setAttribute('href', `results?or=${setLinkOr}`)
+        } else if (setLinkAnd === "" && setLinkOr === ""){
+            searchButton.setAttribute('href', '')
+        } else{
+            searchButton.setAttribute('href', `results?and=${setLinkAnd}&or=${setLinkOr}`)
+        }; 
+    };
+
+
+};
+
+function selectTagToggleOr(tag, inputField, searchButton) {
     const tagValue = encodeURIComponent(tag.value);
     const selectedTags = inputField.value.split('+');
 
